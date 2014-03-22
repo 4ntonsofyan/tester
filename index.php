08bfc0498d884d88c6c031ceb29ddfb757a1e991
4ntonsofyan <4ntonsofyan@gmail.com>
2014-03-22 14:13:20 +0700

    Signed-off-by: 4ntonsofyan <4ntonsofyan@gmail.com>

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Tester Github
+ * Tester
  * @author       Anton Sofyan | http://antonsofyan.com
  * @copyright    (c) Oktober 2014
  */
@@ -9,6 +9,11 @@
 class Tester
 {
 	function __construct()
+	{
+		# code...
+	}
+
+	public function index()
 	{
 		# code...
 	}
