ab14681f33752a4b802a3638d79743c348b45b6f
4ntonsofyan <4ntonsofyan@gmail.com>
2014-03-22 14:03:18 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Tester Github
+ * @author       Anton Sofyan | http://antonsofyan.com
+ * @copyright    (c) Oktober 2014
+ */
+
+class Tester
+{
+	function __construct()
+	{
+		# code...
+	}
+}
